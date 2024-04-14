2f820a5dfd843d95aa2832c669ce1ea16a484f88
anav5704 <anavkki@gmail.com>
2024-04-14 22:54:24 +1200

    build: initialise project

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native-web"
+
+export default WelcomeScreen = () => {
+    return (
+        <View>
+            <Text>Welcome: React Native + Expo</Text>
+        </View>
+    )
+}
